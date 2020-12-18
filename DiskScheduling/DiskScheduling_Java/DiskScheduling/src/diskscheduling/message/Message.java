5d7ffa81a75ea0834a37fb87118b58aef65d282b
uday12381 <51029317+uday12381@users.noreply.github.com>
2020-12-18 19:39:50 +0530

    Update Message.java

--- DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/message/Message.java
+++ DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/message/Message.java
@@ -1,19 +1,20 @@
 package diskscheduling.message ;
-import java.io.* ;
+import java.io.Serializable ;
+import java.util.Arrays ;
+
 public class Message implements Serializable {
+	
 	public int arr[] , headPosition , lastPosition , algorithm ;
 	public boolean isValid ;
+	
 	public Message(int arr[] , int headPosition , int lastPosition , int algorithm) {
-		this.arr = new int[arr.length] ;
-		System.out.println(arr.length) ;
-		for (int i = 0 ; i < arr.length ; i++) {
-			this.arr[i] = arr[i] ;
-		}
+		this.arr = Arrays.copyOfRange(arr , 0 , arr.length) ;
 		this.headPosition = headPosition ;
 		this.lastPosition = lastPosition ;
 		this.algorithm = algorithm ;
 		isValid = true ;
 	}
+	
 	public String toString() {
 		return "" ;
 	}
