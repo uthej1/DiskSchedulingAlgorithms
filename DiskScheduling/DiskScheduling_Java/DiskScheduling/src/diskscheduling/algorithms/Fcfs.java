b366a1fb086c13f45de8d1e27d50706fb6dc0f99
uday12381 <51029317+uday12381@users.noreply.github.com>
2020-12-18 19:46:26 +0530

    Update Fcfs.java

--- DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/algorithms/Fcfs.java
+++ DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/algorithms/Fcfs.java
@@ -1,11 +1,15 @@
 package diskscheduling.algorithms ;
 public class Fcfs {
+	
 	public static int[] fcfs(int diskRequests[] , int headPosition , int lastPosition) {
 		int order[] = new int[diskRequests.length + 1] ;
 		order[0] = headPosition ;
+		
 		for (int i = 0 ;  i < diskRequests.length ; i++) {
 			order[i + 1] = diskRequests[i] ;
 		}
+		
 		return order ;
 	}
+	
 }
