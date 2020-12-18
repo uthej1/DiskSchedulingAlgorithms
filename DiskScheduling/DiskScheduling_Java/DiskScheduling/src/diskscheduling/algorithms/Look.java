d095686a42a3cac2803b4f1e7908f5a157558499
uday12381 <51029317+uday12381@users.noreply.github.com>
2020-12-18 19:47:14 +0530

    Update Look.java

--- DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/algorithms/Look.java
+++ DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/algorithms/Look.java
@@ -1,8 +1,11 @@
 package diskscheduling.algorithms ;
-import java.util.* ;
+import java.util.Arrays ;
+import java.util.ArrayList ;
 public class Look {
+	
 	private static int lowerBound(int arr[] , int val) {
 		int ans = arr.length , left = 0 , right = arr.length - 1 ;
+		
 		while (left <= right) {
 			int mid = (left + right) / 2 ;
 			if (arr[mid] < val) {
@@ -12,13 +15,16 @@ public class Look {
 				ans = mid ;
 			}
 		}
+		
 		return ans ;
 	}
+	
 	public static int[] look(int diskRequests[] , int headPosition , int lastPosition) {
 		ArrayList<Integer> orderAl = new ArrayList<Integer>() ;
 		orderAl.add(headPosition) ;
 		Arrays.sort(diskRequests) ;
 		int idx = lowerBound(diskRequests , headPosition) ;
+		
 		for (int i = idx ; i < diskRequests.length ; i++) {
 			orderAl.add(diskRequests[i]) ;
 		}
@@ -35,4 +41,5 @@ public class Look {
 
 		return order ;
 	}
+	
 }
