0352b757f7d2786f9bb179ee75750d8b86d5c6a6
uday12381 <51029317+uday12381@users.noreply.github.com>
2020-12-18 19:42:49 +0530

    Update Client.java

--- DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/client/Client.java
+++ DiskScheduling/DiskScheduling_Java/DiskScheduling/src/diskscheduling/client/Client.java
@@ -1,16 +1,20 @@
 package diskscheduling.client ;
 import javafx.application.Application ; 
-import java.io.* ;
+import java.io.ObjectOutputStream ;
+import java.io.ObjectInputStream ;
 import java.net.* ;
-import java.util.* ;
-
+import java.util.Scanner ;
 import diskscheduling.graph.Graph;
-import diskscheduling.message.* ;
+import diskscheduling.message.Message ;
+
 public class Client {
+	
 	public static void main(String args[]) throws Exception {
+		
 		Scanner ip = new Scanner(System.in) ;
 		System.out.println("Welcome client") ;
 		int port = 3378 ;
+		
 		System.out.println("Enter\n1.First Come First Server\n2.Shortest Seek Time First\n3.SCAN\n4.CSCAN\n5.LOOK\n6.CLOOK") ;
 		int type = ip.nextInt() ;
 		System.out.println("Enter the head initial position") ;
@@ -20,16 +24,20 @@ public class Client {
 		System.out.println("Enter the requests") ;
 		String c = ip.nextLine() ;
 		String str[] = ip.nextLine().split(" ") ;
+		
 		int arr[] = new int[str.length] ;
 		for (int i = 0 ; i < str.length ; i++) {
 			arr[i] = Integer.parseInt(str[i]) ;
 		}
+		
 		Message obj = new Message(arr , head , last , type) ;
 		Socket client = new Socket("localhost" , port) ;
 		ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream()) ;
 		ObjectInputStream ois = new ObjectInputStream(client.getInputStream()) ;
+		
 		oos.writeObject(obj) ;
 		obj = (Message) ois.readObject() ;
+		
 		if (obj.isValid) {
 			int x[] = Arrays.copyOfRange(obj.arr, 0, obj.arr.length) ; 
 			int y[] = new int[x.length] ;
